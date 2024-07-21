cf642556f0839bb20d49adfaa75d54350474832a
Ola <seakayit@gmail.com>
2024-07-21 20:49:23 +0000

    adding other scripts for class

---
+++ greet.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "good morning,$(whoami)"
+echo "You're currently on the $(hostname) server"
+echo "This server has been online for about  $(uptime)"
+echo "Todays date is  $(date)"
+friend="Yinka is my friend"
+echo "Who is my friend: $friend"
